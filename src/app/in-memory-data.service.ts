5b94a05c3cbfcb08248a1d2e63e74fe3986c2b6a
Asad Rizvi <rizv06@gmail.com>
2020-01-08 10:54:18 -0500

    load heroes from web API, add post, put, delete to HeroService and components, add search box and delete button

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,22 +1,10 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class InMemoryDataService {
-
-//   constructor() { }
-// }
-
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import {Hero} from './hero';
-import {Injectable} from '@angular/core';
-import { HeroesComponent } from './heroes/heroes.component';
+import { Hero } from './hero';
+import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
@@ -29,11 +17,17 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 17, name: 'Dynama' },
       { id: 18, name: 'Dr IQ' },
       { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }      
+      { id: 20, name: 'Tornado' }
     ];
     return {heroes};
   }
+
+  // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (11).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
   genId(heroes: Hero[]): number {
-    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1: 11;
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
   }
 }
